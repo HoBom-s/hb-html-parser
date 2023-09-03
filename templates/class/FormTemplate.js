1acac21a6994ca1256a87981dfb98d63652e0e94
foxmon <jjockrod@naver.com>
2023-09-03 21:51:10 +0900

    Form Template

---
+++ templates/class/FormTemplate.js
@@ -0,0 +1,51 @@
+import { HoBomHTMLParserBase } from "../../src/core/hobomHtmlParserBase.js";
+
+export class FormTemplateParser extends HoBomHTMLParserBase {
+  constructor() {
+    super();
+
+    this.templateId = "TEMPLATE";
+    this.templateName = "FormTemplate";
+    this.templateNode = this.matchNode("FormTemplate");
+  }
+}
+
+export class FormGroupParser extends HoBomHTMLParserBase {
+  constructor() {
+    super();
+
+    this.templateId = "GROUP";
+    this.templateName = "FormGroup";
+    this.templateNode = this.matchNode("FormGroup");
+  }
+}
+
+export class InputListParser extends HoBomHTMLParserBase {
+  constructor() {
+    super();
+
+    this.templateId = "LIST";
+    this.templateName = "InputList";
+    this.templateNode = this.matchNode("InputList");
+  }
+}
+
+export class InputUserNameItemParser extends HoBomHTMLParserBase {
+  constructor() {
+    super();
+
+    this.templateId = "ITEM";
+    this.templateName = "InputUserNameItem";
+    this.templateNode = this.matchNode("InputUserNameItem");
+  }
+}
+
+export class InputUserPasswordItemParser extends HoBomHTMLParserBase {
+  constructor() {
+    super();
+
+    this.templateId = "ITEM";
+    this.templateName = "InputUserPasswordItem";
+    this.templateNode = this.matchNode("InputUserPasswordItem");
+  }
+}
